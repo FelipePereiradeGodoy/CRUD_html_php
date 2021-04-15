e7bd450ce250f75a3f0f165cff9e22091ea7f782
FelipePereiraDeGodoy <godoypereirafelipe@gmail.com>
2021-04-15 12:17:35 -0300

    TESTANDO

--- Controller/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $login = $_POST['login'];
-    $senha = $_POST['senha'];
-    $entrar = $_POST['entrar'];
-
-    if(isset($entrar)) {
-        $bancoMysql->verificaLoginSenha($login, $senha);
-    }
-?>
--- Controller/recebeCadastroCliente.php
+++ Controller/recebeCadastroCliente.php
@@ -1,19 +1,20 @@
 <?php
-    require_once("../Model/Cliente.php");
-    require_once("./ControllerBD.php");
+    echo 'TESTE';
+    //require '/opt/lampp/htdocs/CRUD_html_php/Model/Cliente.php';
+    //require '/opt/lampp/htdocs/CRUD_html_php/Controller/ControllerBD.php';
     
-    ClCliente $c = ClCliente();
+    //ClCliente $c = new ClCliente();
     
-    $c->nome = POST['nome'];
-    $c->cpf = POST['cpf'];
-    $c->rg =  POST['rg'];
-    $c->email = POST['email'];
-    $c->endereco = POST['endereco'];
-    $c->telefone1 = POST['telefone1'];
-    $c->telefone2 = POST['telefone2'];
-    $c-> dataNasc = POST['dataNasc'];
+    //$c->nome = $_POST['nome'];
+    //$c->cpf = $_POST['cpf'];
+    //$c->rg =  $_POST['rg'];
+    //$c->email = $_POST['email'];
+    //$c->endereco = $_POST['endereco'];
+    //$c->telefone1 = $_POST['telefone1'];
+    //$c->telefone2 = $_POST['telefone2'];
+    //$c-> dataNasc = $_POST['dataNasc'];
 
-    ClControllerBD $control = ClControllerBD();
-    $control.insereCliente($c);
+    //ClControllerBD $control = new ClControllerBD();
+    //$control.insereCliente($c);
     
 ?>
