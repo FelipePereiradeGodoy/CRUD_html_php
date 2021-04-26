722f257a00c9adcbe30634771cc4327d2284d40b
FelipePereiraDeGodoy <godoypereirafelipe@gmail.com>
2021-04-26 15:05:52 -0300

    Ajustado botao e css para pagina de cadastro de clientes

--- View/page-cadastro-clientes/cadastroCliente.php
+++ View/page-cadastro-clientes/cadastroCliente.php
@@ -17,109 +17,90 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="cadastroCliente.css">
+    <script type="text/javascript" src="../../JS/buscaCliente.js"></script>
 
     <title>Cadastro Cliente</title>
 </head>
 
 <body>
+    <div class="page-cadastro-cliente">
+        <form action="../../Controller/recebeCadastroCliente.php" method="POST">
 
-    <form id="form-block" action="../../Controller/recebeCadastroCliente.php" method="POST">
-        <div id="form-center">
+            <div class="input-btnVoltar-block">
+                <a href="#" onclick="voltarListaCliente()">
+                    <img id='imgSetaEsquerda' src="../../img/arrow-left-square.svg" alt="Seta indicando para voltar">
+                </a>
+            </div>
+
             <input type="hidden" name="idCliente" id="idCliente" value="">
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="nome">Nome Completo:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="nome" id="nome" class="inputText">
-                </div>
+                <input type="text" name="nome" id="nome" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="cpf">CPF:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="cpf" id="cpf" class="inputText">
-                </div>
+                <input type="text" name="cpf" id="cpf" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="rg">RG:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="rg" id="rg" class="inputText">
-                </div>
+                <input type="text" name="rg" id="rg" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="email">Email:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="email" id="email" class="inputText">
-                </div>
+                <input type="text" name="email" id="email" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="endereco">Endereço:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="endereco" id="endereco" class="inputText">
-                </div>
+                <input type="text" name="endereco" id="endereco" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="telefone1">Telefone 1:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="telefone1" id="telefone1" class="inputText">
-                </div>
+                <input type="text" name="telefone1" id="telefone1" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="telefone2">Telefone 2:</label>
-
-                <div class="col-sm-10">
-                    <input type="text" name="telefone2" id="telefone2" class="inputText">
-                </div>
+                <input type="text" name="telefone2" id="telefone2" class="inputText">
 
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-block">
 
                 <label for="dataNasc">Data Nascimento:</label>
-
-                <div class="col-sm-10">
-                    <input type="date" name="dataNasc" id="dataNasc">
-                </div>
+                <input type="date" name="dataNasc" id="dataNasc">
 
             </div>
 
-            <div class="col-sm-10 offset-sm-2" id="div-row-mb-3">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="isAtivo" id="isAtivo" checked>
-                    <label class="form-check-label" for="gridCheck1">
-                        Cliente Ativo
-                    </label>
-                </div>
+            <div class="input-checkbox-block">
+                <input type="checkbox" name="isAtivo" id="isAtivo" checked>
+                <label for="gridCheck1">
+                    Cliente Ativo
+                </label>
             </div>
 
-            <div class="row mb-3" id="div-row-mb-3">
+            <div class="input-button-block">
                 <button type="submit" class="btn btn-primary" id="btn-Salvar">Salvar</button>
             </div>
-        </div>
+    </div>
     </form>
 
 </body>
